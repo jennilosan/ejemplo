ed43206a7988ca4d09a46a9c996033b92a7bb2eb
jennilosan <jennilopezsan15@gmail.com>
2025-09-30 13:19:22 -0600

    Dockerfile corregido y funcionando

--- src/lib/nameGenerator.js
+++ src/lib/nameGenerator.js
@@ -1,44 +1,22 @@
 // src/lib/nameGenerator.js
-const racePrefixes = {
+
+export const racePrefixes = {
   Human: ['Jon', 'Ari', 'Mar', 'Cal', 'Eli', 'Dar', 'Sam', 'Tom', 'Nia', 'Leo'],
   Elf: ['Ela', 'Fin', 'Gal', 'Sil', 'Ael', 'Tho', 'Lir', 'Vel', 'Zir', 'Nil'],
   Dwarf: ['Bor', 'Dur', 'Thro', 'Gim', 'Kil', 'Uth', 'Var', 'Bri', 'Kaz', 'Mol']
 };
 
-const classSuffixes = {
-  Warrior: [
-    'the Strong',
-    'Shieldbearer',
-    'of the Blade',
-    'Ironfist',
-    'Battleborn',
-    'the Relentless'
-  ],
+export const classSuffixes = {
+  Warrior: ['the Strong', 'Shieldbearer', 'of the Blade', 'Ironfist', 'Battleborn', 'the Relentless'],
   Mage: ['Fireweaver', 'Arclight', 'the Spellbound', 'the Mystic', 'Stormbringer', 'Voidwalker'],
-  Rogue: [
-    'of the Shadows',
-    'Whispercloak',
-    'the Quick',
-    'Nightstalker',
-    'Daggerfall',
-    'Silentblade'
-  ]
+  Rogue: ['of the Shadows', 'Whispercloak', 'the Quick', 'Nightstalker', 'Daggerfall', 'Silentblade']
 };
 
-function generateName(race, charClass) {
+export function generateName(race, charClass) {
   const prefix = racePrefixes[race][Math.floor(Math.random() * racePrefixes[race].length)];
-  const suffix =
-    classSuffixes[charClass][Math.floor(Math.random() * classSuffixes[charClass].length)];
+  const suffix = classSuffixes[charClass][Math.floor(Math.random() * classSuffixes[charClass].length)];
   return `${prefix} ${suffix}`;
 }
 
-const availableRaces = Object.keys(racePrefixes);
-const availableClasses = Object.keys(classSuffixes);
-
-module.exports = {
-  generateName,
-  availableRaces,
-  availableClasses,
-  racePrefixes,
-  classSuffixes
-};
+export const availableRaces = Object.keys(racePrefixes);
+export const availableClasses = Object.keys(classSuffixes);
